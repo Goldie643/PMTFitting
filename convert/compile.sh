aa763e330db9496d798af0224ea6328a5e3768e7
Alex Goldsack <alex@NMESs-MacBook-Pro.local>
2022-11-02 15:07:42 +0000

    Update compile prog to us C++14, remove extra includes not needed.

--- convert/compile.sh
+++ convert/compile.sh
@@ -1,5 +1,6 @@
-#export BOOST=/path/to/boost
+export BOOST=/opt/homebrew/Cellar/boost/1.80.0/include
 
-g++  -O2 -Wall -fPIC -pthread -std=c++11 -m64 -I$BOOST -I$ROOTSYS/include -c main.cpp
-g++  -O2 -m64 -std=c++11 main.o -lm -L$ROOTSYS/lib -lCore -lRIO -lNet -lHist -lGraf -lGraf3d -lGpad -lTree -lRint -lPostscript -lMatrix -lPhysics -lMathCore -lThread -lMultiProc -pthread -lm -ldl -rdynamic -o waveconvert
+g++  -std=c++14 -O2 -Wall -fPIC -pthread -m64 -I$BOOST -I$ROOTSYS/include -I$ROOTSYS/include/root -c main.cpp 
+#g++  -O2 -m64 -std=c++11 main.o -lm -L$ROOTSYS/lib -lCore -lRIO -lNet -lHist -lGraf -lGraf3d -lGpad -lTree -lRint -lPostscript -lMatrix -lPhysics -lMathCore -lThread -lMultiProc -pthread -lm -ldl -rdynamic -o waveconvert
+g++  -O2 -m64 -std=c++14 main.o -lm $(root-config --libs) -rdynamic -o waveconvert
 rm main.o
